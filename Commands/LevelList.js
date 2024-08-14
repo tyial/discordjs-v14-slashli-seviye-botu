ce00f321a4f01f3963b4c2f17e1374e2cc5a1520
Furkan <132396838+tyial@users.noreply.github.com>
2024-08-14 15:57:35 +0300

    Update LevelList.js

--- Commands/LevelList.js
+++ Commands/LevelList.js
@@ -309,231 +309,232 @@ module.exports = {
       const page = await browser.newPage();
 
       const htmlContent = `
-    <!DOCTYPE html>
-    <html lang="tr">
-    <head>
-      <meta charset="UTF-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Seviye Listesi</title>
-      <style>
-        body {
-          font-family: Arial, sans-serif;
-          color: #ffffff;
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-          justify-content: center;
-          margin: 0;
-          padding: 20px;
-        }
-    
-        .container {
-          display: flex;
-          flex-direction: column;
-          align-items: center;
-        }
-    
-        .top-three {
-          display: flex;
-          justify-content: center;
-          width: 100%;
-          max-width: 600px;
-          margin-bottom: 20px;
-        }
-    
-        .top-three .user {
-          text-align: center;
-          padding: 10px;
-          margin: 0 5px;
-          border-radius: 10px;
-          background-color: #23272a;
-          border: 2px solid white;
-          background: radial-gradient(circle at bottom right, rgba(219, 219, 219, 0.5), transparent);
-        }
-    
-        .top-three .user:nth-child(1) {
-          background-color: #FFD700;
-          order: 2;
-          width: 150px;
-        }
-    
-        .top-three .user:nth-child(2) {
-          background-color: silver;
-          order: 1;
-          width: 120px;
-        }
-    
-        .top-three .user:nth-child(3) {
-          background-color: #cd7f32;
-          order: 3;
-          width: 120px;
-        }
-    
-        .top-three .user img {
-          border-radius: 50%;
-          width: 80px;
-          height: 80px;
-        }
-    
-        .user h3 {
-          font-size: 16px;
-          font-weight: bold;
-          overflow: hidden;
-          text-overflow: ellipsis;
-          white-space: nowrap;
-          max-width: 100px;
-        }
-    
-        .user p {
-          font-size: 14px;
-          white-space: nowrap;
-        }
-    
-        .top-three .user .rank-number {
-          font-size: 24px;
-          color: #ffffff;
-          font-weight: bold;
-          top: 5px;
-          left: 5px;
-        }
-    
-        .rankings {
-          display: grid;
-          grid-template-columns: 1fr 1fr;
-          gap: 20px;
-          width: 100%;
-          max-width: 600px;
-          margin-top: 20px;
-        }
-    
-        .ranking {
-          display: flex;
-          align-items: center;
-          padding: 10px;
-          border-radius: 10px;
-          background-color: #23272a;
-          border: 2px solid white;
-          background: radial-gradient(circle at top left, rgba(21, 125, 125, 0.5), transparent);
-        }
-    
-        .ranking img {
-          border-radius: 50%;
-          width: 40px;
-          height: 40px;
-          margin-right: 10px;
-        }
-    
-        .ranking .info {
-          display: flex;
-          flex-direction: column;
-          overflow: hidden;
-          white-space: nowrap;
-          text-overflow: ellipsis;
-        }
-    
-        .ranking h4 {
-          margin: 0;
-          font-size: 16px;
-          font-weight: bold;
-          overflow: hidden;
-          text-overflow: ellipsis;
-          white-space: nowrap;
-          max-width: 100px;
-        }
-    
-        .ranking p {
-          margin: 2px 0;
-          font-size: 14px;
-        }
-    
-        .ranking .rank-number {
-          font-size: 24px;
-          color: #ffffff;
-          font-weight: bold;
-          margin-right: 10px;
-        }
-    
-        .bottom-center {
-          display: flex;
-          justify-content: center;
-          width: 100%;
-          margin-top: 10px;
-        }
-    
-        .bottom-center .ranking {
-          display: flex;
-          align-items: center;
-          padding: 15px;
-          border-radius: 10px;
-          background-color: #23272a;
-          border: 2px solid white;
-          background: radial-gradient(circle at top left, rgba(21, 125, 125, 0.5), transparent);
-        }
-    
-        .bottom-center .ranking img {
-          border-radius: 50%;
-          width: 50px;
-          height: 50px;
-          margin-left: 10px;
-          margin-right: 10px;
-        }
-    
-        .bottom-center .ranking.centered {
-          justify-content: center;
-        }
-    
-        .bottom-center .ranking.centered .rank-number {
-          margin-right: 0;
-        }
-    
-        .bottom-center .ranking h4 {
-          margin: 0;
-          font-size: 18px;
-          font-weight: bold;
-          overflow: hidden;
-          text-overflow: ellipsis;
-          white-space: nowrap;
-          max-width: 120px;
-        }
-      </style>
-    </head>
-    <body>
-      <div class="container">
-        <div class="top-three">
-          ${users.slice(0, 3).map((user, index) => `
-            <div class="user" style="background-color: ${index === 0 ? '#FFD700' : index === 1 ? 'silver' : '#cd7f32'};">
-              <img src="${user.avatar}" alt="${user.username}">
-              <h3>${user.username}</h3>
-              <p>${user.level}. seviye, ${user.xp} xp</p>
-              <p class="rank-number">#${index + 1}</p>
-            </div>
-          `).join('')}
-        </div>
-        <div class="rankings">
-          ${users.slice(3, 9).map((user, index) => `
-            <div class="ranking">
-              <p class="rank-number">#${index + 4}</p>
-              <img src="${user.avatar}" alt="${user.username}">
-              <div class="info">
-                <h4>${user.username}</h4>
+      <!DOCTYPE html>
+      <html lang="tr">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Seviye Listesi</title>
+        <style>
+          body {
+            font-family: Arial, sans-serif;
+            color: #ffffff;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            margin: 0;
+            padding: 20px;
+          }
+      
+          .container {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+          }
+      
+          .top-three {
+            display: flex;
+            justify-content: center;
+            width: 100%;
+            max-width: 600px;
+            margin-bottom: 20px;
+          }
+      
+          .top-three .user {
+            text-align: center;
+            padding: 10px;
+            margin: 0 5px;
+            border-radius: 10px;
+            background-color: #23272a;
+            border: 2px solid white;
+            background: radial-gradient(circle at bottom right, rgba(219, 219, 219, 0.5), transparent);
+          }
+      
+          .top-three .user:nth-child(1) {
+            background-color: #FFD700;
+            order: 2;
+            width: 150px;
+          }
+      
+          .top-three .user:nth-child(2) {
+            background-color: silver;
+            order: 1;
+            width: 120px;
+          }
+      
+          .top-three .user:nth-child(3) {
+            background-color: #cd7f32;
+            order: 3;
+            width: 120px;
+          }
+      
+          .top-three .user img {
+            border-radius: 50%;
+            width: 80px;
+            height: 80px;
+          }
+      
+          .user h3 {
+            font-size: 16px;
+            font-weight: bold;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            white-space: nowrap;
+            max-width: 100px;
+          }
+      
+          .user p {
+            font-size: 14px;
+            white-space: nowrap;
+          }
+      
+          .top-three .user .rank-number {
+            font-size: 24px;
+            color: #ffffff;
+            font-weight: bold;
+            top: 5px;
+            left: 5px;
+          }
+      
+          .rankings {
+            display: grid;
+            grid-template-columns: 1fr 1fr;
+            gap: 20px;
+            width: 100%;
+            max-width: 600px;
+            margin-top: 20px;
+          }
+      
+          .ranking {
+            display: flex;
+            align-items: center;
+            padding: 10px;
+            border-radius: 10px;
+            background-color: #23272a;
+            border: 2px solid white;
+            background: radial-gradient(circle at top left, rgba(21, 125, 125, 0.5), transparent);
+          }
+      
+          .ranking img {
+            border-radius: 50%;
+            width: 40px;
+            height: 40px;
+            margin-right: 10px;
+          }
+      
+          .ranking .info {
+            display: flex;
+            flex-direction: column;
+            overflow: hidden;
+            white-space: nowrap;
+            text-overflow: ellipsis;
+          }
+      
+          .ranking h4 {
+            margin: 0;
+            font-size: 16px;
+            font-weight: bold;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            white-space: nowrap;
+            max-width: 100px;
+          }
+      
+          .ranking p {
+            margin: 2px 0;
+            font-size: 14px;
+          }
+      
+          .ranking .rank-number {
+            font-size: 24px;
+            color: #ffffff;
+            font-weight: bold;
+            margin-right: 10px;
+          }
+      
+          ${users[9] ? `.bottom-center {
+            display: flex;
+            justify-content: center;
+            width: 100%;
+            margin-top: 10px;
+          }
+      
+          .bottom-center .ranking {
+            display: flex;
+            align-items: center;
+            padding: 15px;
+            border-radius: 10px;
+            background-color: #23272a;
+            border: 2px solid white;
+            background: radial-gradient(circle at top left, rgba(21, 125, 125, 0.5), transparent);
+          }
+      
+          .bottom-center .ranking img {
+            border-radius: 50%;
+            width: 50px;
+            height: 50px;
+            margin-left: 10px;
+            margin-right: 10px;
+          }
+      
+          .bottom-center .ranking.centered {
+            justify-content: center;
+          }
+      
+          .bottom-center .ranking.centered .rank-number {
+            margin-right: 0;
+          }
+      
+          .bottom-center .ranking h4 {
+            margin: 0;
+            font-size: 18px;
+            font-weight: bold;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            white-space: nowrap;
+            max-width: 120px;
+          }` : ''}
+        </style>
+      </head>
+      <body>
+        <div class="container">
+          <div class="top-three">
+            ${users.slice(0, 3).map((user, index) => `
+              <div class="user" style="background-color: ${index === 0 ? '#FFD700' : index === 1 ? 'silver' : '#cd7f32'};">
+                <img src="${user.avatar}" alt="${user.username}">
+                <h3>${user.username}</h3>
                 <p>${user.level}. seviye, ${user.xp} xp</p>
+                <p class="rank-number">#${index + 1}</p>
+              </div>
+            `).join('')}
+          </div>
+          <div class="rankings">
+            ${users.slice(3, 9).map((user, index) => `
+              <div class="ranking">
+                <p class="rank-number">#${index + 4}</p>
+                <img src="${user.avatar}" alt="${user.username}">
+                <div class="info">
+                  <h4>${user.username}</h4>
+                  <p>${user.level}. seviye, ${user.xp} xp</p>
+                </div>
+              </div>
+            `).join('')}
+          </div>
+          ${users[9] ? `
+          <div class="bottom-center">
+            <div class="ranking centered">
+              <p class="rank-number">#10</p>
+              <img src="${users[9].avatar}" alt="${users[9].username}">
+              <div class="info">
+                <h4>${users[9].username}</h4>
+                <p>${users[9].level}. seviye, ${users[9].xp} xp</p>
               </div>
             </div>
-          `).join('')}
+          </div>` : ''}
         </div>
-        <div class="bottom-center">
-          <div class="ranking centered">
-            <p class="rank-number">#10</p>
-            <img src="${users[9]?.avatar}" alt="${users[9]?.username}">
-            <div class="info">
-              <h4>${users[9]?.username}</h4>
-              <p>${users[9]?.level}. seviye, ${users[9]?.xp} xp</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </body>
-    </html>`;
+      </body>
+      </html>`;
 
       await page.setContent(htmlContent);
       await page.setViewport({ width: 550, height: 650 });
